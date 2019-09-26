857906dbf6dc1afcc4bf4cfd2f3ee325f97d60c5
Behzad Najafi <b.najafi87@gmail.com>
2019-09-26 17:37:07 +0200

    Update assignment_guideline.py

--- Assignment1/assignment_guideline.py
+++ Assignment1/assignment_guideline.py
@@ -1,1 +1,2 @@
-#Here are the guidelines for this assignment
+# IN this assignemnt you arre supposed to solve the Example  B of the previous week (double pane window) using the method that we saw to
+# together in class
