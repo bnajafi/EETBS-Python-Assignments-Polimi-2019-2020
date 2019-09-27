8fc811ed6214225c513355ff20ef179528569361
Behzad Najafi <b.najafi87@gmail.com>
2019-09-27 19:05:59 +0200

    Update assignment_guideline.py

--- Assignment1/assignment_guideline.py
+++ Assignment1/assignment_guideline.py
@@ -1,4 +1,4 @@
 
 # In this assignment you are supposed to solve the Example B of the previous week (double pane window: two layers of glass and an air gap )
 # using the method that we saw together in class 
-# Hint: as we discussed you can use a list of lists for representing resistances and use "if statement" to handle cond/conv calculation
+# Hint: as we discussed you can use a list of lists for representing resistances and use "if statement" to handle cond/conv calculations
