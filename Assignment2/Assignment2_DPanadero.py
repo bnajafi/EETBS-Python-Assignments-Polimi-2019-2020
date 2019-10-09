4fe3a665e0173fd99e7963574f57349d83638178
dpanaderoricos <davidpari_94@hotmail.com>
2019-10-09 15:02:06 +0200

    I uploaded assignment 2

---
+++ Assignment2/Assignment2_DPanadero.py
@@ -0,0 +1,85 @@
+#Determine the overall unit thermal resistance (the Rvalue) and the overall heat transfer coefficient (the U-factor) of a wood frame wall that is built around 38-mm 90-mm
+#wood studs with a center-to-center distance of 400 mm. The 90-mm-wide cavity between the studs is filled with
+#glass fiber insulation. The inside is finished with 13-mm gypsum wallboard and the outside with 13-mm wood
+#fiberboard and 13-mm 200-mm wood bevel lapped siding. The insulated cavity constitutes 75 percent of the heat
+#transmission area while the studs, plates, and sills constitute 21 percent. The headers constitute 4 percent of the area, and
+# they can be treated as studs. Also, determine the rate of heat loss through the walls of a
+#house whose perimeter is 50 m and wall height is 2.5 m in Las Vegas, Nevada, whose winter design temperature is -2 C. Take the indoor design temperature to be 22 C and
+#assume 20 percent of the wall area is occupied by glazing.
+
+
+
+#Library of Resistance values of each material
+ThermalResDict={"GlassFiberInsulation":{"R":0.7,"length":0.025},
+                "WoodStud_90mm":{"R":0.63,"length":0.09},
+                "WoodFiberBoard":{"R":0.23,"length":0.013},
+                "WoodLappedSiding":{"R":0.14,"length":0.013},
+                "Gypsum":{"R":0.079,"length":0.013},
+                "InsideSurface":{"R":0.12},
+                "OutsideSurface":{"R":0.03},#WINTER
+                }
+
+#Dictionaries of every Resistance
+R_i={"name":"inside surface", "type":"conv", "material":"InsideSurface"}
+R_2={"name":"Wood bevel lapped Siding", "type":"cond", "material":"WoodLappedSiding", "length":0.013}
+R_3={"name":"Wood fiberboard", "type":"cond", "material":"WoodFiberBoard", "length":0.013}
+R_4={"name":"Glass fiber insulation", "type":"cond", "material":"GlassFiberInsulation", "length":0.09}
+R_5={"name":"Wood studs", "type":"cond", "material":"WoodStud_90mm", "length":0.09}          
+R_6={"name":"Gypsum Wallboard","type":"cond", "material":"Gypsum", "length":0.013}
+R_o={"name":"Outside surface", "type":"conv", "material":"OutsideSurface"}
+
+#There are in parallel to heat transmition: one passes through the wood stud 25% and the the other through the glass fiber 75%
+
+#R_total_wood
+#In serie this resistances R_i: I=i,2,3,4.a,5,o. Simply sum them. 
+
+ResistanceList_withWood=[R_i,R_2,R_3,R_5,R_6,R_o]
+Rtot_withWood=0
+
+for AnyResistance in ResistanceList_withWood:
+    if AnyResistance["type"]=="cond":
+        material_AnyResistance=AnyResistance["material"]
+        length_AnyResistance=AnyResistance["length"]
+        lengthOnLibrary=ThermalResDict[material_AnyResistance]["length"]
+        RValue_AnyResistance=ThermalResDict[material_AnyResistance]["R"]*length_AnyResistance/lengthOnLibrary
+        AnyResistance["RValue"]=RValue_AnyResistance
+        Rtot_withWood=Rtot_withWood+RValue_AnyResistance
+    elif AnyResistance["type"]=="conv":
+        material_AnyResistance=AnyResistance["material"]
+        RValue_AnyResistance=ThermalResDict[material_AnyResistance]["R"]
+        AnyResistance["RValue"]=RValue_AnyResistance
+        Rtot_withWood=Rtot_withWood+RValue_AnyResistance
+
+#R_total_glass
+#In serie this resistances R_i: I=i,2,3,4.b,5,o. Simply sum them.
+
+ResistanceList_withInsulation=[R_i,R_2,R_3,R_4,R_6,R_o]
+Rtot_withInsulation=0
+
+for AnyResistance in ResistanceList_withInsulation:
+    if AnyResistance["type"]=="cond":
+        material_AnyResistance=AnyResistance["material"]
+        length_AnyResistance=AnyResistance["length"]
+        lengthOnLibrary=ThermalResDict[material_AnyResistance]["length"]
+        RValue_AnyResistance=ThermalResDict[material_AnyResistance]["R"]*length_AnyResistance/lengthOnLibrary
+        AnyResistance["RValue"]=RValue_AnyResistance
+        Rtot_withInsulation=Rtot_withInsulation+RValue_AnyResistance
+    elif AnyResistance["type"]=="conv":
+        material_AnyResistance=AnyResistance["material"]
+        RValue_AnyResistance=ThermalResDict[material_AnyResistance]["R"]
+        AnyResistance["RValue"]=RValue_AnyResistance
+        Rtot_withInsulation=Rtot_withInsulation+RValue_AnyResistance
+
+U_wood=1/Rtot_withWood
+U_insulation=1/Rtot_withInsulation
+Utotal=0.25*U_wood+0.75*U_insulation
+print("The total heat transfer coefficient is Utotal= "+str(Utotal))
+Rtotal=1/Utotal
+print("The total thermal resistance is Rtotal= "+str(Rtotal))
+
+#Rate of heat loss through the walls
+Area=50*2.5*0.8
+T_1=22
+T_2=-2
+Q=Utotal*Area*(T_1-T_2)
+print("The rate of heat loss through the walls is Q= "+str(Q))
