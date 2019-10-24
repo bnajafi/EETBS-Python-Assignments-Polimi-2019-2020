126ab2a20195e75a226fbb77ee1ca5591a8612df
Behzad Najafi <b.najafi87@gmail.com>
2019-10-24 17:35:41 +0200

    Update assignment_guideline.py

--- Assignment5/assignment_guideline.py
+++ Assignment5/assignment_guideline.py
@@ -1,1 +1,3 @@
 #Here are the guidelines for this assignment
+In this assignment you should solve the same examplle as that of assignment 2 and this time you should do that using numpy !
+You are not allowed to use for and if 
